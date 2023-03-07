d28dc381293d48a37dbc00aba3536627480dc553
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-03-07 15:02:20 +0100

    add class-component-user

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,15 @@
-import classes from './User.module.css';
+import { Component } from "react/cjs/react.production.min";
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+import classes from "./User.module.css";
+
+class User extends Component {
+  render() {
+    return <li className={classes.user}>{props.name}</li>;
+  }
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
